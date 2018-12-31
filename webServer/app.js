28bcbc92ee22ee9e94ae28c612c4d6b18cb2ae62
kgthree <kevinguilbault@gmail.com>
2018-12-31 17:05:11 -0500

    updated vocab and web server app

--- webServer/app.js
+++ webServer/app.js
@@ -3,16 +3,21 @@ var fs = require('fs');
 
 http .createServer(function(req, res) {
 
-  /*
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  fs.createReadStream(`${__dirname}/index.htm`).pipe(res);
-  */
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  var obj =  {
-    firstName: 'John',
-    lastName: 'Doe'
-  };
-  res.end(JSON.stringify(obj));
-
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream(`${__dirname}/index.htm`).pipe(res);
+  }
+  else if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    var obj =  {
+      firstName: 'John',
+      lastName: 'Doe'
+    };
+    res.end(JSON.stringify(obj));
+  }
+  else {
+    res.writeHead(404);
+    res.end('Error 404: The page you requested does not exist.');
+  }
 
 }).listen(1337, '127.0.0.1');
