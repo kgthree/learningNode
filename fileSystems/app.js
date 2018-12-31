19cd8e3505147d1b6f32817a40dad8f2619c0b13
kgthree <kevinguilbault@gmail.com>
2018-12-30 21:16:28 -0500

    Updated vocab and added File System project

---
+++ fileSystems/app.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+// synchronous approach
+var greet = fs.readFileSync(`${__dirname}/greet.txt`, 'utf8');
+console.log(greet);
+
+// asynchronous approach
+var greet2 = fs.readFile(`${__dirname}/greet.txt`, 'utf8',
+  function(err, data){
+    console.log(data);
+  });
+
+console.log('Done!');
