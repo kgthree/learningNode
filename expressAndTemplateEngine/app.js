14182931b5c80dba6a592bdbdb6b2400fa3fd625
kgthree <kevinguilbault@gmail.com>
2019-01-01 13:28:25 -0500

    updated express app with db connection info

--- expressAndTemplateEngine/app.js
+++ expressAndTemplateEngine/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mysql = require ('mysql');
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
@@ -12,8 +13,23 @@ app.set('view engine', 'ejs');
 
 app.use('/', function(req,res,next) {
   console.log('Request Url: ' + req.url);
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "",
+    password: "",
+    database: "test_database"
+  });
+
+  con.query('SELECT * FROM People.new_table;',
+    function(err, rows) {
+      if(err) throw err;
+        console.log(rows[0].firstName);
+    }
+  );
   next();
 });
+
+
 
 htmlController(app);
 
